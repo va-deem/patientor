e282a9a8b24ceb376b897713063e1d8fccd71d0d
Vadim S <vadim.stebakov@gmail.com>
2021-06-06 20:35:38 +0300

    Complete 9.20: Display entries

--- src/PatientPage/index.tsx
+++ src/PatientPage/index.tsx
@@ -3,9 +3,9 @@ import axios from 'axios';
 import {apiBaseUrl} from "../constants";
 import {useStateValue, addPatient} from "../state";
 
-import {Patient} from '../types';
+import {Patient, Entry} from '../types';
 import {useParams} from 'react-router-dom';
-import {Container, List, Icon} from "semantic-ui-react";
+import {Container, List, ListItem, Icon} from "semantic-ui-react";
 
 const PatientPage = () => {
   const {id} = useParams<{ id: string }>();
@@ -29,15 +29,33 @@ const PatientPage = () => {
 
   if (!patients[id]) return null;
 
+  const renderEntry = (entry: Entry): JSX.Element => {
+    return (<ListItem>
+      {`${entry.date} ${entry.description}`}
+      {entry.diagnosisCodes?.length ?
+        (<List>
+          {entry.diagnosisCodes.map(code =>
+            (<ListItem key={code}>{code}</ListItem>)
+          )}
+        </List>) : []}
+    </ListItem>);
+  };
+
   return <div className="App">
     <Container textAlign="center">
       <h3>{patients[id].name}</h3>
     </Container>
-    <List bulleted>
+    <List>
       <List.Item>Id: {patients[id].id}</List.Item>
       <List.Item>Gender: {patients[id].gender === 'male' ?
         <Icon name="mars" /> : <Icon name="venus" />}</List.Item>
+      <List.Item>SSN: {patients[id].ssn}</List.Item>
       <List.Item>Occupation: {patients[id].occupation}</List.Item>
+      <ListItem>
+        <List bulleted>
+          {patients[id].entries.map((entry) => renderEntry(entry))}
+        </List>
+      </ListItem>
     </List>
   </div>;
 };
--- src/types.ts
+++ src/types.ts
@@ -17,4 +17,52 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[]
 }
+
+export interface DiagnoseEntry {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry['code']>
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type HospitalDischarge = {
+  date: string;
+  criteria: string;
+};
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: HospitalDischarge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
